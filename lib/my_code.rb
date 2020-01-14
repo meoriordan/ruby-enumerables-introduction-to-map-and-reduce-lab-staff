ed0d7017abda51963ec626cf1c1e344fc0c2f6eb
Elizabeth ORiordan <elizabeth.oriordan@flatironschool.com>
2020-01-14 01:53:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,59 @@
 # My Code here....
+def map_to_negativize(source_array)
+  new_array = []
+  for item in source_array
+    new_array << -item 
+  end
+  new_array
+end
+
+def map_to_no_change(source_array)
+  new_array = []
+  for item in source_array
+    new_array << item 
+  end
+  new_array
+end
+
+def map_to_double(source_array)
+  new_array = []
+  for item in source_array
+    new_array << (item * 2)
+  end
+  new_array
+end
+
+def map_to_square(source_array)
+  new_array = []
+  for item in source_array
+    new_array << (item**2)
+  end
+  new_array
+end
+
+def reduce_to_total(source_array, starting_point=0)
+  for item in source_array
+    starting_point += item 
+  end
+  starting_point
+end
+
+def reduce_to_all_true(source_array)
+  for item in source_array
+    if item == FALSE
+      return FALSE
+    end
+  end
+  return TRUE
+end
+
+def reduce_to_any_true(source_array)
+  x = FALSE
+  for item in source_array
+    if item == TRUE
+      x = TRUE
+      return x 
+    end
+  end
+  x
+end
